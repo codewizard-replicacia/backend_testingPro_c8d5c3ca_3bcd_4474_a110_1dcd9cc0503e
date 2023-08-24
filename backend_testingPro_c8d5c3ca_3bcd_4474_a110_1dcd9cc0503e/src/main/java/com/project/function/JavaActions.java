b8c92d822fcc04f25481f31c89c36dd3dc9a106e
Administrator <Administrator@172.31.4.67>
2023-08-24 09:32:09 +0000

    initial commit

---
+++ backend_testingPro_c8d5c3ca_3bcd_4474_a110_1dcd9cc0503e/src/main/java/com/project/function/JavaActions.java
@@ -0,0 +1,28 @@
+package com.project.function;
+
+
+
+
+
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmAction;
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmParameter;
+import com.sap.olingo.jpa.metadata.core.edm.mapper.extension.ODataAction;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.transaction.Transactional;
+
+@Component
+public class JavaActions implements ODataAction {
+    private final EntityManager entityManager;
+
+    public JavaActions(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+	
+	
+}
+  
---
+++ backend_testingPro_c8d5c3ca_3bcd_4474_a110_1dcd9cc0503e/src/main/java/com/project/function/JavaFunctions.java
@@ -0,0 +1,19 @@
+package com.project.function;
+
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmFunction;
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmParameter;
+import com.sap.olingo.jpa.metadata.core.edm.mapper.extension.ODataFunction;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JavaFunctions implements ODataFunction {
+
+
+    
+    
+}
+   
